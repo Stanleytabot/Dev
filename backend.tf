dd30e78e46451e36dac91f69178c40012b41bcea
Stanleytabot <99697986+Stanleytabot@users.noreply.github.com>
2023-01-23 08:51:17 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,13 @@
 terraform {
   backend "s3" {
-    bucket = "hotep"
+    bucket = "Hotep-backend"
     key    = "path/to/my/key"
     region = "us-east-2"
+  cloud {
+    organization = "FusionIT"
+
+    workspaces {
+      name = "git-hub-action"
+    }
   }
 }
